143a30a60817182dac68a82c1f202dcaa252e846
ugurdonmez <ugurdonmez87@gmail.com>
2016-12-09 23:05:06 +0100

    add calender page link.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,23 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { CalenderPage } from '../pages/calender/calender';
 
 @NgModule({
-  declarations: [
-    MyApp,
-    HomePage
-  ],
-  imports: [
-    IonicModule.forRoot(MyApp)
-  ],
-  bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    HomePage
-  ],
-  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
+    declarations: [
+        MyApp,
+        HomePage,
+        CalenderPage
+    ],
+    imports: [
+        IonicModule.forRoot(MyApp)
+    ],
+    bootstrap: [IonicApp],
+    entryComponents: [
+        MyApp,
+        HomePage,
+        CalenderPage
+    ],
+    providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,14 +2,22 @@ import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
 
+import { CalenderPage } from '../calender/calender';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+    constructor(public navCtrl: NavController) {
     
-  }
+    }
+    
+    openCalender(page) {
+        
+        this.navCtrl.push(CalenderPage);
+    }
 
 }
