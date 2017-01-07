eac74df055fdc29d1ed49f67927e3243e40952cd
ugurdonmez <ugurdonmez87@gmail.com>
2017-01-07 20:18:56 +0100

    school-admin-add-update-branch page added.

--- src/pages/school-admin-add-update-branch/school-admin-add-update-branch.ts
+++ src/pages/school-admin-add-update-branch/school-admin-add-update-branch.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { ClassModel } from '../../models/class-model';
+
+
 @Component({
   selector: 'page-school-admin-add-update-branch',
   templateUrl: 'school-admin-add-update-branch.html'
@@ -8,12 +11,50 @@ import { NavController } from 'ionic-angular';
 
 export class SchoolAdminAddUpdateBranchPage {
 
-    constructor(public navCtrl: NavController) {}
+    classes: Array<ClassModel> = []
+
+    constructor(public navCtrl: NavController) {
+        this.importClassesMock();
+    }
 
     ionViewDidLoad() {
         console.log('Hello SchoolAdminAddUpdateBranchPage Page');
     }
 
+    importClassesMock() {
+        let c1: ClassModel = {
+            id: 'class1',
+            name: 'Class A',
+            teacher_id: 'teacher1',
+            teacher_name: 'Ali Donmez',
+            age: 5,
+            current: 0,
+            maximum: 10
+        };
 
+        let c2: ClassModel = {
+            id: 'class1',
+            name: 'Class A',
+            teacher_id: 'teacher1',
+            teacher_name: 'Ali Donmez',
+            age: 5,
+            current: 0,
+            maximum: 10
+        };
+
+        let c3: ClassModel = {
+            id: 'class1',
+            name: 'Class A',
+            teacher_id: 'teacher1',
+            teacher_name: 'Ali Donmez',
+            age: 5,
+            current: 0,
+            maximum: 10
+        };
+
+        this.classes.push(c1);
+        this.classes.push(c2);
+        this.classes.push(c3);
+    }
 
 }
