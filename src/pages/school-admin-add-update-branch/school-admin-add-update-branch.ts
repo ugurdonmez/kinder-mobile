9ae4acc477b5d46b1c228b31287a71753ebdf3f0
borakrc <borakrc@gmail.com>
2017-01-08 18:48:42 +0300

    addBranch() firebase connection implemented

--- src/pages/school-admin-add-update-branch/school-admin-add-update-branch.ts
+++ src/pages/school-admin-add-update-branch/school-admin-add-update-branch.ts
@@ -1,17 +1,38 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Branches } from '../../providers/branches';
+import {BranchModel} from '../../models/branch-model';
 
 @Component({
   selector: 'page-school-admin-add-update-branch',
-  templateUrl: 'school-admin-add-update-branch.html'
+  templateUrl: 'school-admin-add-update-branch.html',
+    providers: [Branches, BranchModel]
 })
 
 export class SchoolAdminAddUpdateBranchPage {
+    mockBranchModel: BranchModel;
 
-    constructor(public navCtrl: NavController) {}
+
+    constructor(public navCtrl: NavController, public branches: Branches) {
+        this.mockBranchModel = new BranchModel();
+        this.mockBranchModel.name = "mockName";
+        this.mockBranchModel.manager = "mockmanager";
+        this.mockBranchModel.manager_tel = "mockmanager_tel";
+        this.mockBranchModel.address = "mockaddress";
+        this.mockBranchModel.classes = [
+            "mocksample1",
+            "mocksample2",
+            "mocksample3",
+        ];
+    }
 
     ionViewDidLoad() {
         console.log('Hello SchoolAdminAddUpdateBranchPage Page');
+    }
+
+    addNewBranch(){
+        //console.log("addNewBranch() triggered.");
+        this.branches.addBranch(this.mockBranchModel);
     }
 
 
--- src/providers/branches.ts
+++ src/providers/branches.ts
@@ -1,14 +1,16 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
 
+import { AngularFire } from 'angularfire2';
 import {BranchModel} from '../models/branch-model';
 
 @Injectable()
 export class Branches {
 
-    constructor(public http: Http) {
-        console.log('Hello Branches Provider');
+    branches: any;
+
+    constructor(public af: AngularFire) {
+        this.branches = af.database.list('/branches');
     }
 
     public getUserBranches() {
@@ -16,11 +18,12 @@ export class Branches {
     }
 
     public getBranch(branchId: string) {
-
+        // branch = this.branches.object('/branches/'+branchId);
+        // console.log(branch.name)
     }
 
     public addBranch(branch: BranchModel) {
-
+        this.branches.push(branch)
     }
 
     public updateBranch(branch: BranchModel) {
