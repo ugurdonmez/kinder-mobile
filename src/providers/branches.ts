d43aeba2d10f624f03301d376b37c953606687d9
ugurdonmez <ugurdonmez87@gmail.com>
2017-01-11 22:31:05 +0100

    method updated.

--- src/providers/branches.ts
+++ src/providers/branches.ts
@@ -22,8 +22,7 @@ export class Branches {
     }
 
     public getBranch(branchId: string) {
-        var fetchedBranch = this.af.database.object('/branches/' + branchId);
-        return fetchedBranch;
+        return this.af.database.object('/branches/' + branchId);
     }
 
     public addBranch(branch: BranchModel) {
