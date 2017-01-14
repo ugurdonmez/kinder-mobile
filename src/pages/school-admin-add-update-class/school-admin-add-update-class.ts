9e80d3a59b44943a6fbb12d1b80e07b0c864f56e
borakrc <borakrc@gmail.com>
2017-01-14 22:49:27 +0300

    small bugfix for tests of Class Provider

--- src/pages/school-admin-add-update-class/school-admin-add-update-class.ts
+++ src/pages/school-admin-add-update-class/school-admin-add-update-class.ts
@@ -12,9 +12,9 @@ import { Classes } from '../../providers/classes';
 })
 
 export class SchoolAdminAddUpdateClassPage {
-    randomMockClass: ClassModel = new ClassModel();
+    randomMockClass: ClassModel;
 
-    constructor(public navCtrl: NavController, public classesProvider: Classes) {
+    constructor(public navCtrl: NavController, public _class: Classes) {
     }
 
     ionViewDidLoad() {
@@ -22,25 +22,26 @@ export class SchoolAdminAddUpdateClassPage {
     }
 
     addNewClass(){
-        //addClass test
-        this.generateRandomMockClass();
-        this.classesProvider.addClass(this.randomMockClass);
-
         //getClass test
-        console.log(this.classesProvider.getClass("-KaSzyajwg5kuPNpFRpp"));
+        console.log(this._class.getClass("-KaSzyajwg5kuPNpFRpp"));
 
         //updateClass test
         this.generateRandomMockClass();
         this.randomMockClass.id = "-KaSzyajwg5kuPNpFRpp";
-        this.classesProvider.updateClass(this.randomMockClass);
+        this._class.updateClass(this.randomMockClass);
+
+        //addClass test
+        this.generateRandomMockClass();
+        this._class.addClass(this.randomMockClass);
 
         //getUserClasses test
-        this.classesProvider.getUserClasses(function(userClassArray){
+        this._class.getUserClasses(function(userClassArray){
             console.log(userClassArray)
         })
     }
 
     private generateRandomMockClass() {
+        this.randomMockClass = new ClassModel();
         this.randomMockClass.name = (function(n){return eval("["+Array(n).join("String.fromCharCode(65+~~(Math.random()*61)),")+",'']").join("");}(8));
         this.randomMockClass.teacher_id = "mockTeacherId";
         this.randomMockClass.teacher_name = "Mock Ogretmen";
