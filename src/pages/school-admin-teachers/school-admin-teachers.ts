a0528acb468752d57dbfc0bfbcc966c53cb671c6
borakrc <borakrc@gmail.com>
2017-01-17 02:51:25 +0300

    teacher provider tests using frontend

--- src/pages/school-admin-teachers/school-admin-teachers.ts
+++ src/pages/school-admin-teachers/school-admin-teachers.ts
@@ -2,22 +2,46 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 import { TeacherModel } from '../../models/teacher-model';
+import {Teachers} from "../../providers/teachers";
 
 @Component({
   selector: 'page-school-admin-teachers',
-  templateUrl: 'school-admin-teachers.html'
+  templateUrl: 'school-admin-teachers.html',
+    providers: [Teachers]
 })
 export class SchoolAdminTeachersPage {
 
-    teachers: Array<TeacherModel> = []
+    teachers: Array<TeacherModel> = [];
+    randomMockTeacher: TeacherModel;
 
-    constructor(public navCtrl: NavController) {
+    constructor(public navCtrl: NavController, public teacher: Teachers) {
         this.importTeachersMock();
     }
 
     ionViewDidLoad() {
         console.log('Hello SchoolAdminTeachersPage Page');
     }
+
+    addNewTeacher(){
+        //getTeacher test
+        console.log(this.teacher.getTeacher("-KadHidAEYTRGT-48rzP"));
+
+        //updateTeacher test
+        this.generateRandomMockTeacher();
+        this.randomMockTeacher.id = "-KadHidAEYTRGT-48rzP";
+        this.teacher.updateTeacher(this.randomMockTeacher);
+
+        //addTeacher test
+        this.generateRandomMockTeacher();
+        this.teacher.addTeacher(this.randomMockTeacher);
+
+        // TODO ugur hoca'nin github'da issue #6'ya verecegi yanita gore geri eklenebilir
+        //getUserTeachers test
+        // this.teacher.getUserTeachers(function(userTeacherArray){
+        //     console.log(userTeacherArray)
+        // })
+    }
+
 
     importTeachersMock() {
         let t1: TeacherModel = {
@@ -46,4 +70,14 @@ export class SchoolAdminTeachersPage {
         this.teachers.push(t2);
     }
 
+    private generateRandomMockTeacher() {
+        this.randomMockTeacher = new TeacherModel();
+
+        this.randomMockTeacher.name = (function(n){return eval("["+Array(n).join("String.fromCharCode(65+~~(Math.random()*61)),")+",'']").join("");}(8));
+        this.randomMockTeacher.surname = (function(n){return eval("["+Array(n).join("String.fromCharCode(65+~~(Math.random()*61)),")+",'']").join("");}(8));
+        this.randomMockTeacher.branches = ["mockBranchId1", "mockBranchId2", "mockBranchId3"];
+        this.randomMockTeacher.classes = ["mockClassId1", "mockClassId2"];
+        this.randomMockTeacher.notes = "some notes here";
+        this.randomMockTeacher.photo_url = "http://cliparts.co/cliparts/qiB/AgG/qiBAgGrjT.jpg";
+    }
 }
