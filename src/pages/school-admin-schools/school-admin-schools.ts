d207a2a5f26d7702bcf22b1bfbb437201266e242
borakrc <borakrc@gmail.com>
2017-02-05 07:27:06 +0300

    branch page now lists schools of that branch.

--- src/pages/school-admin-schools/school-admin-schools.ts
+++ src/pages/school-admin-schools/school-admin-schools.ts
@@ -15,27 +15,27 @@ import {SchoolAdminAddUpdateSchoolPage} from "../school-admin-add-update-school/
 
 export class SchoolAdminSchoolsPage {
     private branchId: string;
-    private allSchools: any;
+    private allSchoolsOfBranch: any;
     private branch: FirebaseObjectObservable<any>;
 
     constructor(public navCtrl: NavController, public schoolsProvider: Schools, public branchesProvider: Branches,
                 private navParams: NavParams) {
         this.branchId = navParams.get('branchId');
         this.branch = branchesProvider.getBranch(this.branchId);
-        this.allSchools = this.schoolsProvider.getAllSchools();
+        this.allSchoolsOfBranch = this.schoolsProvider.getSchoolsOfBranch(this.branchId);
     }
 
     ionViewDidLoad() {
         console.log('Hello SchoolAdminSchoolsPage Page');
     }
 
-    // openSchoolPage(schoolId){
-    //     console.log(schoolId);
-    //     // this.navCtrl.push()
-    // }
-    //
-    openSchoolAdminSchoolAdd(page) {
-        console.log('open school admin school');
-        this.navCtrl.push(SchoolAdminAddUpdateSchoolPage, {'branchId':this.branchId});
+    openSchoolPage(schoolId){
+        console.log('goes to class list of that school with schoolId:' + schoolId);
+        // this.navCtrl.push()
+    }
+
+    openSchoolAdminSchoolAdd() {
+        console.log('adds new school to branch with branchId: ' + this.branchId);
+        // this.navCtrl.push( school ekleme  , {'schoolId':this.schoolId});
     }
 }
