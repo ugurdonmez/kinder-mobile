29426a01daa9fd887a535f63e291b2615d307c34
borakrc <borakrc@gmail.com>
2017-02-05 07:07:49 +0300

    clicking on a branch now navitages user to branch's page.

--- src/pages/school-admin-branches/school-admin-branches.ts
+++ src/pages/school-admin-branches/school-admin-branches.ts
@@ -5,6 +5,7 @@ import { BranchModel } from '../../models/branch-model';
 import {Branches} from '../../providers/branches'
 
 import { SchoolAdminAddUpdateBranchPage } from '../school-admin-add-update-branch/school-admin-add-update-branch';
+import { SchoolAdminSchoolsPage} from "../school-admin-schools/school-admin-schools";
 
 
 @Component({
@@ -26,92 +27,10 @@ export class SchoolAdminBranchesPage {
         console.log('Hello SchoolAdminBranchesPage Page');
     }
 
-    // importBranchesMock() {
-    //
-    //     let b1: BranchModel = { id: 'id1',
-    //                             name: 'branch ankara',
-    //                             manager: 'ahmet donmez',
-    //                             manager_tel: '05052223344',
-    //                             address: 'cankaya 10. sok ankara',
-    //                             classes: ['cid1', 'cid2', 'cid3']
-    //                           };
-    //
-    //     let b2: BranchModel = { id: 'id2',
-    //                             name: 'branch ankara',
-    //                             manager: 'ahmet donmez',
-    //                             manager_tel: '05052223344',
-    //                             address: 'cankaya 10. sok ankara',
-    //                             classes: ['cid1', 'cid2', 'cid3']
-    //                           };
-    //
-    //     let b3: BranchModel = { id: 'id3',
-    //                             name: 'branch ankara',
-    //                             manager: 'ahmet donmez',
-    //                             manager_tel: '05052223344',
-    //                             address: 'cankaya 10. sok ankara',
-    //                             classes: ['cid1', 'cid2', 'cid3']
-    //                           };
-    //
-    //     let b4: BranchModel = { id: 'id3',
-    //                             name: 'branch ankara',
-    //                             manager: 'ahmet donmez',
-    //                             manager_tel: '05052223344',
-    //                             address: 'cankaya 10. sok ankara',
-    //                             classes: ['cid1', 'cid2', 'cid3']
-    //                           };
-    //
-    //     let b5: BranchModel = { id: 'id3',
-    //                             name: 'branch ankara',
-    //                             manager: 'ahmet donmez',
-    //                             manager_tel: '05052223344',
-    //                             address: 'cankaya 10. sok ankara',
-    //                             classes: ['cid1', 'cid2', 'cid3']
-    //                           };
-    //
-    //     let b6: BranchModel = { id: 'id3',
-    //                             name: 'branch ankara',
-    //                             manager: 'ahmet donmez',
-    //                             manager_tel: '05052223344',
-    //                             address: 'cankaya 10. sok ankara',
-    //                             classes: ['cid1', 'cid2', 'cid3']
-    //                           };
-    //
-    //     let b7: BranchModel = { id: 'id3',
-    //                             name: 'branch ankara',
-    //                             manager: 'ahmet donmez',
-    //                             manager_tel: '05052223344',
-    //                             address: 'cankaya 10. sok ankara',
-    //                             classes: ['cid1', 'cid2', 'cid3']
-    //                           };
-    //
-    //     let b8: BranchModel = { id: 'id3',
-    //                             name: 'branch ankara',
-    //                             manager: 'ahmet donmez',
-    //                             manager_tel: '05052223344',
-    //                             address: 'cankaya 10. sok ankara',
-    //                             classes: ['cid1', 'cid2', 'cid3']
-    //                           };
-    //
-    //     let b9: BranchModel = { id: 'id3',
-    //                             name: 'branch ankara',
-    //                             manager: 'ahmet donmez',
-    //                             manager_tel: '05052223344',
-    //                             address: 'cankaya 10. sok ankara',
-    //                             classes: ['cid1', 'cid2', 'cid3']
-    //                           };
-    //
-    //
-    //     this.branches.push(b1);
-    //     this.branches.push(b2);
-    //     this.branches.push(b3);
-    //     this.branches.push(b4);
-    //     this.branches.push(b5);
-    //     this.branches.push(b6);
-    //     this.branches.push(b7);
-    //     this.branches.push(b8);
-    //     this.branches.push(b9);
-    //
-    // }
+    openBranchPage(branchId){
+        console.log(branchId);
+        this.navCtrl.push(SchoolAdminSchoolsPage, {'branchId':branchId});
+    }
 
     openSchoolAdminBranchUpdate(page) {
         console.log('open school admin branch');
