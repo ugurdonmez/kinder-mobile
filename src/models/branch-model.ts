0ec05800d97499dd997ec76d6c714b4ef296c224
borakrc <borakrc@gmail.com>
2017-02-06 04:21:17 +0300

    modified branch-model. added branch_telephone and manager_mail

--- src/models/branch-model.ts
+++ src/models/branch-model.ts
@@ -1,8 +1,10 @@
 export class BranchModel {
     id: string;
     name: string;
+    tel: string;
     manager: string;
     manager_tel: string;
+    manager_mail: string;
     address: string;
     classes: Array<string>;
 }
