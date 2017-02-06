279eff82510f02e52d13f8a1cd6f475e70e241c4
borakrc <borakrc@gmail.com>
2017-02-06 04:24:41 +0300

    Add-Branch page completed (/w localization)

--- src/pages/school-admin-add-update-branch/school-admin-add-update-branch.ts
+++ src/pages/school-admin-add-update-branch/school-admin-add-update-branch.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {NavController, AlertController} from 'ionic-angular';
 import { Branches } from '../../providers/branches';
 import {BranchModel} from '../../models/branch-model';
 
 import { ClassModel } from '../../models/class-model';
 import {Translator} from "../../app/translator";
 import {TranslateService} from "ng2-translate";
+import {FormBuilder, Validators} from "@angular/forms";
 
 
 @Component({
@@ -15,14 +16,23 @@ import {TranslateService} from "ng2-translate";
 })
 
 export class SchoolAdminAddUpdateBranchPage {
-    mockBranch: BranchModel = new BranchModel();
     classes: Array<ClassModel> = [];
     translate: TranslateService;
+    branchDetailsForm: any;
 
-    constructor(public navCtrl: NavController, public branches: Branches, public translator: Translator) {
+    constructor(public navCtrl: NavController, public branches: Branches, public formBuilder: FormBuilder,
+                public translator: Translator, public alertCtrl: AlertController) {
         this.translate = translator.translatePipe;
-        this.importClassesMock();
-        this.importBranchMock();
+        this.branchDetailsForm = formBuilder.group(
+            {
+                'name': ['', Validators.required],
+                'tel': ['', Validators.required],
+                'manager': ['', Validators.required],
+                'manager_mail': ['', Validators.required],
+                'manager_tel': ['', Validators.required],
+                'address': ['', Validators.required]
+            }
+        );
     }
 
     ionViewDidLoad() {
@@ -31,54 +41,20 @@ export class SchoolAdminAddUpdateBranchPage {
 
     addNewBranch(){
         //console.log("addNewBranch() triggered.");
-        this.branches.addBranch(this.mockBranch);
-    }
+        // this.branches.addBranch(this.mockBranch);
 
-    importClassesMock() {
-        let c1: ClassModel = {
-            id: 'class1',
-            name: 'Class A',
-            teacher_id: 'teacher1',
-            teacher_name: 'Ali Donmez',
-            age: 5,
-            current: 0,
-            maximum: 10
-        };
+        if (this.branchDetailsForm.valid){
+            this.branches.addBranch(this.branchDetailsForm.value);
+            this.navCtrl.pop();
+        }
+        else{
+            let alert = this.alertCtrl.create({
+                title: 'Cannot Submit!',
+                subTitle: 'At least one of the fields are not valid.',
+                buttons: ['OK']
+            });
+            alert.present();
+        }
 
-        let c2: ClassModel = {
-            id: 'class1',
-            name: 'Class A',
-            teacher_id: 'teacher1',
-            teacher_name: 'Ali Donmez',
-            age: 5,
-            current: 0,
-            maximum: 10
-        };
-
-        let c3: ClassModel = {
-            id: 'class1',
-            name: 'Class A',
-            teacher_id: 'teacher1',
-            teacher_name: 'Ali Donmez',
-            age: 5,
-            current: 0,
-            maximum: 10
-        };
-
-        this.classes.push(c1);
-        this.classes.push(c2);
-        this.classes.push(c3);
-    }
-
-    importBranchMock() {
-        this.mockBranch.name = "mockName";
-        this.mockBranch.manager = "mockmanager";
-        this.mockBranch.manager_tel = "mockmanager_tel";
-        this.mockBranch.address = "mockaddress";
-        this.mockBranch.classes = [
-            "mocksample1",
-            "mocksample2",
-            "mocksample3",
-        ];
     }
 }
