5f81dcfacdc6d12e64adcd20002105d2af6e699c
borakrc <borakrc@gmail.com>
2017-03-26 13:54:52 +0300

    helper class: random string generator

---
+++ src/helpers/randomStringGenerator.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class RandomStringGenerator{
+
+    // generates random string A-Z
+    public get(length){
+        let randomString = eval("["+Array(length).join("String.fromCharCode(65+~~(Math.random()*26)),")+",'']").join("");
+        console.log("generated random string: " + randomString);
+        return randomString;
+    }
+}
+
