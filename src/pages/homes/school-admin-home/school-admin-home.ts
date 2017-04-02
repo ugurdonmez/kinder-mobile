596a52d829854dd966d4500cc7c2214c822b3687
Ugur Donmez <ugurdonmez87@gmail.com>
2017-04-02 20:14:28 +0200

    reformat branches provider

--- src/pages/homes/school-admin-home/school-admin-home.ts
+++ src/pages/homes/school-admin-home/school-admin-home.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { AuthData } from "../../../providers/auth-data";
 import { LoginPage } from "../../login/login";
+import {Branches} from "../../../providers/branches";
 
 
 @Component({
@@ -13,8 +14,26 @@ export class SchoolAdminHomePage {
 
   constructor(public navCtrl: NavController,
               public navParams: NavParams,
-              public authData: AuthData) {
+              public authData: AuthData,
+              private branchesProvider: Branches) {
 
+     this.branchesProvider.getUserBranches().subscribe(snapshot => {
+
+        console.log('school admin page branch')
+        console.log(snapshot)
+
+        if (snapshot.length > 0){
+           console.log("user has branch.");
+        }
+        else{
+           console.log("user has no branch.")
+        }
+     })
+
+     //let branches = branchProvider.getUserBranches()
+
+     console.log('list of school admin branches')
+     // console.log(branches)
   }
 
   ionViewDidLoad() {
--- src/providers/branches.ts
+++ src/providers/branches.ts
@@ -1,9 +1,9 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import 'rxjs/add/operator/map';
 
-import { AngularFire } from 'angularfire2';
+import {AngularFire} from 'angularfire2';
 import {BranchModel} from '../models/branch-model';
-import { AuthData } from './auth-data';
+import {AuthData} from './auth-data';
 
 import {Camera} from "ionic-native";
 import {AlertController} from "ionic-angular";
@@ -16,117 +16,115 @@ import {Schools} from "./schools";
 @Injectable()
 export class Branches {
 
-    branches: any;
-    private translate: TranslateService;
+   branches: any;
+   private translate: TranslateService;
 
-    constructor(public af: AngularFire, public authData: AuthData, private alertCtrl: AlertController,
-                public translator: Translator, private schoolsProvider: Schools){
-        this.branches = af.database.list('/branches');
-        this.translate = translator.translatePipe;
-    }
+   constructor(public af: AngularFire,
+               public authData: AuthData,
+               private alertCtrl: AlertController,
+               public translator: Translator,
+               private schoolsProvider: Schools) {
 
-    public getUserBranches() {
-        var userId = this.authData.getUserId();
+      this.branches = af.database.list('/branches');
+      this.translate = translator.translatePipe;
+   }
 
-        // var user_branch_ids = this.af.database.list('/user-branches/'+userId);
-        // // console.log("user_branch_ids:");
-        // // console.log(user_branch_ids);
-        //
-        // return user_branch_ids;
+   public getUserBranches() {
+      var userId = this.authData.getUserId();
 
-        return this.af.database.list('/branches', {
-            query: {
-                orderByChild: 'adminUserId',
-                equalTo: userId
-            }
-        });
-    }
+      return this.af.database.list('/branches', {
+         query: {
+            orderByChild: 'adminUserId',
+            equalTo: userId
+         }
+      });
+   }
 
-    public getBranch(branchId: string) {
-        return this.af.database.object('/branches/' + branchId);
-    }
+   public getBranch(branchId: string) {
+      return this.af.database.object('/branches/' + branchId);
+   }
 
-    public getAllBranches(){
-        return this.af.database.list('/branches/');
-    }
+   public getAllBranches() {
+      return this.af.database.list('/branches/');
+   }
 
-    public addBranch(branch: BranchModel) {
-        branch.adminUserId = this.authData.getUserId();
-        return this.branches.push(branch).key;
-        // var pushedBranch = this.branches.push(branch);
-        // var branchId = pushedBranch.key;
-        //console.log("branchId: " + branchId);
+   public addBranch(branch: BranchModel) {
+      branch.adminUserId = this.authData.getUserId();
+      return this.branches.push(branch).key;
+      // var pushedBranch = this.branches.push(branch);
+      // var branchId = pushedBranch.key;
+      //console.log("branchId: " + branchId);
 
-        // var userId = this.authData.getUserId();
-        // var user_branches = this.af.database.list('/user-branches/'+userId);
-        // user_branches.push({'branchId':branchId});
-    }
+      // var userId = this.authData.getUserId();
+      // var user_branches = this.af.database.list('/user-branches/'+userId);
+      // user_branches.push({'branchId':branchId});
+   }
 
-    public updateBranch(branch: BranchModel) {
-        this.af.database.object('/branches/'+branch.id).set(branch);
-    }
+   public updateBranch(branch: BranchModel) {
+      this.af.database.object('/branches/' + branch.id).set(branch);
+   }
 
-    addSchoolToBranch(branchId: string, schoolId: string) {
-        // schoollar zaten branch'larini tutuyor, buna gerek yok o yuzden.
-        // duplicate data olunca duzenlemeler, silmeler eklemeler biraz zor oluyor.
-        // let branchSchoolsList = this.af.database.list('/branches/' + branchId + '/schools');
-        // branchSchoolsList.push(schoolId);
-    }
+   addSchoolToBranch(branchId: string, schoolId: string) {
+      // schoollar zaten branch'larini tutuyor, buna gerek yok o yuzden.
+      // duplicate data olunca duzenlemeler, silmeler eklemeler biraz zor oluyor.
+      // let branchSchoolsList = this.af.database.list('/branches/' + branchId + '/schools');
+      // branchSchoolsList.push(schoolId);
+   }
 
-    deleteBranch(branchId: string){
-        this.af.database.object('/branches/' + branchId).remove();
-        var schoolsOfBranch = this.af.database.list('/schools', {
-            query: {
-                orderByChild: 'branchId',
-                equalTo: branchId
-            }
-        });
-        schoolsOfBranch.subscribe(snapshots => {
-            snapshots.forEach(school => {
-                var schoolId = school.$key;
-                this.schoolsProvider.deleteSchool(schoolId);
-            })
-        })
-    }
+   deleteBranch(branchId: string) {
+      this.af.database.object('/branches/' + branchId).remove();
+      var schoolsOfBranch = this.af.database.list('/schools', {
+         query: {
+            orderByChild: 'branchId',
+            equalTo: branchId
+         }
+      });
+      schoolsOfBranch.subscribe(snapshots => {
+         snapshots.forEach(school => {
+            var schoolId = school.$key;
+            this.schoolsProvider.deleteSchool(schoolId);
+         })
+      })
+   }
 
-    public newPhoto(branchId:string){
-        {
-            var imageSource;
-            let confirm = this.alertCtrl.create({
-                title: this.translate.instant('Image source?'),
-                message: '',
-                buttons: [
-                    {
-                        text: this.translate.instant('SAVEDPHOTOALBUM'),
-                        handler: () => {
-                            imageSource = Camera.PictureSourceType.SAVEDPHOTOALBUM;
-                            this.uploadImage(imageSource, branchId);
-                        }
-                    },
-                    {
-                        text: this.translate.instant('PHOTOLIBRARY'),
-                        handler: () => {
-                            imageSource = Camera.PictureSourceType.PHOTOLIBRARY;
-                            this.uploadImage(imageSource, branchId);
-                        }
-                    }
-                ]
+   public newPhoto(branchId: string) {
+      {
+         var imageSource;
+         let confirm = this.alertCtrl.create({
+            title: this.translate.instant('Image source?'),
+            message: '',
+            buttons: [
+               {
+                  text: this.translate.instant('SAVEDPHOTOALBUM'),
+                  handler: () => {
+                     imageSource = Camera.PictureSourceType.SAVEDPHOTOALBUM;
+                     this.uploadImage(imageSource, branchId);
+                  }
+               },
+               {
+                  text: this.translate.instant('PHOTOLIBRARY'),
+                  handler: () => {
+                     imageSource = Camera.PictureSourceType.PHOTOLIBRARY;
+                     this.uploadImage(imageSource, branchId);
+                  }
+               }
+            ]
+         });
+         confirm.present();
+      }
+   }
+
+   private uploadImage(imageSource: any, branchId: string): any {
+      Camera.getPicture({sourceType: imageSource}).then((image) => {
+         var imageData = image;
+         var profilePictureRef = firebase.storage().ref('/branch-images/namedById/').child(branchId + "_" + new Date().getDate() + " @ " + new Date().getTime() + ".png");
+         profilePictureRef.putString(imageData, 'base64', {contentType: 'image/png'})
+            .then((savedPicture) => {
+               this.af.database.object('/branches/' + branchId + "/logoURL")
+                  .set(savedPicture.downloadURL);
             });
-            confirm.present();
-        }
-    }
-
-    private uploadImage(imageSource: any, branchId:string): any{
-        Camera.getPicture({sourceType : imageSource}).then((image) => {
-            var imageData = image;
-            var profilePictureRef = firebase.storage().ref('/branch-images/namedById/').child(branchId+"_"+new Date().getDate() + " @ " + new Date().getTime() + ".png");
-            profilePictureRef.putString(imageData, 'base64', {contentType: 'image/png'})
-                .then((savedPicture) => {
-                    this.af.database.object('/branches/'+branchId+"/logoURL")
-                        .set(savedPicture.downloadURL);
-                });
-        }, (err) => {
-            // Handle error
-        });
-    }
+      }, (err) => {
+         // Handle error
+      });
+   }
 }
