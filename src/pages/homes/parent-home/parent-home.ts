6d35452f08c75894601cce9d425c5ccd4213d0d3
Ugur Donmez <ugurdonmez87@gmail.com>
2017-04-08 15:46:30 +0200

    fix indent.

--- src/pages/homes/parent-home/parent-home.ts
+++ src/pages/homes/parent-home/parent-home.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
-import { AuthData } from "../../../providers/auth-data";
+import {Component} from '@angular/core';
+import {NavController, NavParams} from 'ionic-angular';
+import {AuthData} from "../../../providers/auth-data";
 import {Translator} from "../../../app/translator";
 import {Parents} from "../../../providers/parents";
 import {AddParentPage} from "../../add-parent/add-parent";
@@ -16,99 +16,96 @@ import {MessagePage} from "../../message/message";
 import {CalenderPage} from "../../calender/calender";
 
 @Component({
-  selector: 'page-parent-home',
-  templateUrl: 'parent-home.html',
-    providers: [Translator, Parents]
+   selector: 'page-parent-home',
+   templateUrl: 'parent-home.html',
+   providers: [Translator, Parents]
 })
 export class ParentHomePage {
-    private userClassId: string;
-    private pageTitleTextEn: string = "Parent Home Page";
+   private userClassId: string;
+   private pageTitleTextEn: string = "Parent Home Page";
 
-  constructor(public navCtrl: NavController,
-              public navParams: NavParams,
-              public translator: Translator,
-              public authData: AuthData,
-              private parentsProvider: Parents) {
+   constructor(public navCtrl: NavController,
+               public navParams: NavParams,
+               public translator: Translator,
+               public authData: AuthData,
+               private parentsProvider: Parents) {
       this.parentCheck();
+   }
 
-  }
+   ionViewDidLoad() {
+      console.log('ionViewDidLoad ParentHomePage');
+   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad ParentHomePage');
-  }
+   private parentCheck() {
+      let thisParent = this.parentsProvider.getParent(this.authData.getUserId());
+      thisParent.subscribe(parentSnapshot => {
+         console.log("parent object snapshot:");
+         console.log(parentSnapshot);
+         console.log(parentSnapshot.$value === null);
+         if (parentSnapshot.$value === null) {
+            this.directToCreateParentPage();
+         }
+         else {
+            this.directParentToClassPage();
+         }
+      })
+   }
 
-    private parentCheck() {
-        let thisParent = this.parentsProvider.getParent(this.authData.getUserId());
-        thisParent.subscribe( parentSnapshot => {
-            console.log("parent object snapshot:");
-            console.log(parentSnapshot);
-            console.log(parentSnapshot.$value === null);
-            if(parentSnapshot.$value === null){
-                this.directToCreateParentPage();
-            }
-            else{
-                this.directParentToClassPage();
-            }
-        })
-    }
-
-    private directToCreateParentPage() {
-        this.authData.getUser().subscribe(thisUser => {
-            // console.log("user snapshot:")
-            // console.log(thisUser)
+   private directToCreateParentPage() {
+      this.authData.getUser()
+         .subscribe(thisUser => {
             this.navCtrl.setRoot(AddParentPage, {
-                'classId': thisUser.classId
+               'classId': thisUser.classId
             })
-        })
-    }
+         })
+   }
 
-    private directParentToClassPage() {
-        this.authData.getUser().subscribe(thisUser => {
+   private directParentToClassPage() {
+      this.authData.getUser()
+         .subscribe(thisUser => {
             console.log("opening class details page for parent with object:");
             console.log(thisUser);
             this.userClassId = thisUser.classId;
-        })
-    }
+         })
+   }
 
-    private openMyClass(): any{
-        this.navCtrl.push(SchoolAdminClassDetailsPage, {'classId': this.userClassId});
-    }
+   private openMyClass(): any {
+      this.navCtrl.push(SchoolAdminClassDetailsPage, {'classId': this.userClassId});
+   }
 
-    private openDailyTeacherFeedbackPage(){
-        this.navCtrl.push(DailyTeacherFeedbackPage);
-    }
+   private openDailyTeacherFeedbackPage() {
+      this.navCtrl.push(DailyTeacherFeedbackPage);
+   }
 
-    private openHomeworkPage(){
-        this.navCtrl.push(HomeworksPage);
-    }
+   private openHomeworkPage() {
+      this.navCtrl.push(HomeworksPage);
+   }
 
-    private openMessages(){
-        this.navCtrl.push(MessagePage)
-    }
+   private openMessages() {
+      this.navCtrl.push(MessagePage)
+   }
 
-    openCalender() {
-        this.navCtrl.push(CalenderPage);
-    }
+   openCalender() {
+      this.navCtrl.push(CalenderPage);
+   }
 
-    openWeeklyMenu(){
-        this.navCtrl.push(WeeklyMenuPage);
-    }
+   openWeeklyMenu() {
+      this.navCtrl.push(WeeklyMenuPage);
+   }
 
-    openGallery(){
-        this.navCtrl.push(GalleryPage);
-    }
+   openGallery() {
+      this.navCtrl.push(GalleryPage);
+   }
 
-    openActivityPlan(){
-        this.navCtrl.push(WeeklyActivityPlanPage);
-    }
+   openActivityPlan() {
+      this.navCtrl.push(WeeklyActivityPlanPage);
+   }
 
-    openDailyParentFeedbackPage(){
-        this.navCtrl.push(DailyParentFeedbackPage);
-    }
+   openDailyParentFeedbackPage() {
+      this.navCtrl.push(DailyParentFeedbackPage);
+   }
 
-    openAttendancePage(){
-        this.navCtrl.push(AttendancePage);
-    }
+   openAttendancePage() {
+      this.navCtrl.push(AttendancePage);
+   }
 }
-
-
