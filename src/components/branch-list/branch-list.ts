ec8c34d8f770dfe92c0e66bff338877b49e21933
Ugur Donmez <ugurdonmez87@gmail.com>
2017-04-30 23:05:17 +0200

    small refactoring

--- src/components/branch-list/branch-list.ts
+++ src/components/branch-list/branch-list.ts
@@ -1,20 +1,30 @@
 
 import {Component} from '@angular/core';
+import {Branches} from "../../providers/branches";
+import {Translator} from "../../app/translator";
 
 @Component({
    selector: 'branch-list',
    templateUrl: 'branch-list.html',
-   providers: []
+   providers: [Branches, Translator]
 })
 
 export class BranchListDirective {
 
-   constructor() {
+   constructor(public branchProvider: Branches,
+               public translator: Translator) {
+
+      console.log('BranchListDirective: constructor()')
 
    }
 
    ionViewDidLoad() {
-
+      console.log('BranchListDirective: ionViewDidLoad()')
+      this.branchProvider.getSchoolAdminBranches()
+         .then(res => {
+            console.log('BranchListDirective: branches of school admin ')
+            console.log(res)
+         })
    }
 
 }
--- src/pages/school-admin/branch/branch.ts
+++ src/pages/school-admin/branch/branch.ts
@@ -1,34 +1,23 @@
-
 import {Component} from '@angular/core';
-import {NavController, NavParams} from 'ionic-angular';
 import {Branches} from "../../../providers/branches";
 import {Translator} from "../../../app/translator";
-import {BranchModel} from "../../../models/branch-model";
 
 @Component({
    selector: 'page-school-admin-branch',
    templateUrl: 'branch.html',
-   providers: [Branches, Translator]
+   providers: [Translator]
 })
 
 export class SchoolAdminBranchPage {
 
    private pageTitleTextEn: string = "Branch Admin Home Page";
 
-   constructor(public navCtrl: NavController,
-               public navParams: NavParams,
-               public branchProvider: Branches,
-               public translator: Translator
-   ) {
+   constructor(public translator: Translator) {
 
    }
 
    ionViewDidLoad() {
-      this.branchProvider.getSchoolAdminBranches()
-         .then(res => {
-            console.log('branch model ')
-            console.log(res)
-         })
+
    }
 
 }
