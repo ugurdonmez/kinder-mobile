b798f3d1c5d13357a2257a4b9fbdc839a353f337
borakrc <borakrc@gmail.com>
2017-04-30 16:33:01 +0300

    updated class model for new features: attendance, homework, gallery, class wall and invitations.

--- src/models/class-model.ts
+++ src/models/class-model.ts
@@ -6,4 +6,29 @@ export class ClassModel {
     age: number;
     current: number;
     maximum: number;
+    schoolId: string;
+    attendance: any;
+    gallery: any;
+    homeworks: any;
+    invitations: any;
+    wall: any;
+    wallRead: any;
+
+    public fromObject(obj: any):ClassModel {
+        this.id = obj.id;
+        this.name = obj.name;
+        this.teacher_id = obj.teacher_id;
+        this.teacher_name = obj.teacher_name;
+        this.age = obj.age;
+        this.current = obj.current;
+        this.maximum = obj.maximum;
+        this.schoolId = obj.schoolId;
+        this.attendance = obj.attendance;
+        this.gallery = obj.gallery;
+        this.homeworks = obj.homeworks;
+        this.invitations = obj.invitations;
+        this.wall = obj.wall;
+        this.wallRead = obj.wallRead;
+        return this;
+    }
 }
