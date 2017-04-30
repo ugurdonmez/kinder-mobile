1010d2e9e49fe9ac7d2ad0d84192d451e39de00c
borakrc <borakrc@gmail.com>
2017-04-30 17:24:48 +0300

    updated fields in parent-model

--- src/models/parent-model.ts
+++ src/models/parent-model.ts
@@ -1,7 +1,27 @@
 export class ParentModel {
     id: string;
-    name: string;
-    surname: string;
-    telephone: string;
-    student_id: string;
+    classId: string
+    parentName: string
+    parentSurname: string
+    parentTelephone: string
+    profileImageUrl: string
+    studentAddress: string
+    studentBirthDate: string
+    studentName: string
+    studentSurname: string
+
+    public fromObject(obj: any):ParentModel {
+        this.id = obj.id;
+        this.classId = obj.classId
+        this.parentName = obj.parentName
+        this.parentSurname = obj.parentSurname
+        this.parentTelephone = obj.parentTelephone
+        this.profileImageUrl = obj.profileImageUrl
+        this.studentAddress = obj.studentAddress
+        this.studentBirthDate = obj.studentBirthDate
+        this.studentName = obj.studentName
+        this.studentSurname = obj.studentSurname
+
+        return this;
+    }
 }
