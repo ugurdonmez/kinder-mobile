880e4aeed2091725ff3bd6c320365770a2edcedd
borakrc <borakrc@gmail.com>
2017-05-03 16:11:13 +0300

    birthday provider refactor

---
+++ src/providers/birthday.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+
+import { AngularFire } from 'angularfire2';
+import {AuthData} from "./auth-data";
+import {Parents} from "./parents";
+
+@Injectable()
+export class Birthday {
+
+    constructor(public af: AngularFire, private authDataProvider: AuthData, private parentsProvider: Parents){
+    }
+
+    // TODO if we use subscribe, this function can't return. if not subscribe, we can't query the birthdays.
+    // not using angularfire2 might be a solution. query with ".child('studentBirthDate')" might work in firebase library.
+    // also, we can subscribe here and use callback, but it's redundant.
+    // in this case, retrieving the birthdays should be handled on the front-end.
+    // returns students of class.
+    public getBirthdays(classId){
+        return this.parentsProvider.getParentsOfClass(classId)
+    }
+}
