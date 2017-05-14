29e4dc557b61979b0a705de3a385caec5b87537a
Ugur Donmez <ugurdonmez87@gmail.com>
2017-05-14 23:42:04 +0200

    teacher page init

--- src/pages/teacher/home/home.ts
+++ src/pages/teacher/home/home.ts
@@ -1,14 +1,24 @@
 
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
    selector: 'page-teacher-home',
    templateUrl: 'home.html',
 })
 
-export class TeacherHomePage {
+export class TeacherHomePage implements OnInit {
+
+   private schoolName: string
+   private classLogoURL: string
+   private className: string
 
    constructor() {
 
+   }
+
+   ngOnInit(): void {
+      this.schoolName = 'school name'
+      this.classLogoURL = 'url'
+      this.className = 'class name'
    }
 }
