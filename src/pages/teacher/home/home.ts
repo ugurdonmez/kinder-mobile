2de2d5e3125c75f7013aa9f0fff9eaf847f6cb90
Ugur Donmez <ugurdonmez87@gmail.com>
2017-05-21 16:41:14 +0200

    progress teacher main

--- src/pages/teacher/home/home.ts
+++ src/pages/teacher/home/home.ts
@@ -1,5 +1,6 @@
 
 import {Component, OnInit} from '@angular/core';
+import {StudentModel} from "../../../models/student-model";
 
 @Component({
    selector: 'page-teacher-home',
@@ -12,13 +13,41 @@ export class TeacherHomePage implements OnInit {
    private classLogoURL: string
    private className: string
 
-   constructor() {
+   private students: Array<StudentModel>
 
+   constructor() {
+      this.students = []
    }
 
    ngOnInit(): void {
-      this.schoolName = 'school name'
-      this.classLogoURL = 'url'
-      this.className = 'class name'
+
+      // this is fake now
+      this.schoolName = 'Muhittin Okullari'
+      this.classLogoURL = 'https://tr-static.eodev.com/files/ddd/0c7d58879d3fbd88329301579d3f91a1.jpg'
+      this.className = 'Ari Sinifi'
+
+      // students
+      let student1 = new StudentModel()
+
+      student1.name = 'merve'
+      student1.surname = 'donmez'
+      student1.photo_url = 'http://c12.incisozluk.com.tr/res/incisozluk//11507/8/1020948_o563d.jpg'
+
+      let student2 = new StudentModel()
+
+      student2.name = 'sofia'
+      student2.surname = 'donmez'
+      student2.photo_url = 'http://c12.incisozluk.com.tr/res/incisozluk//11507/8/1020948_o563d.jpg'
+
+      let student3 = new StudentModel()
+
+      student3.name = 'anna'
+      student3.surname = 'donmez'
+      student3.photo_url = 'http://c12.incisozluk.com.tr/res/incisozluk//11507/8/1020948_o563d.jpg'
+
+
+      this.students.push(student1)
+      this.students.push(student2)
+      this.students.push(student3)
    }
 }
