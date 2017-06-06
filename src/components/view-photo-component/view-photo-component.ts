c80e0d979dbbf3b81a7f1cd4b8e1167e87d82aaa
borakrc <borakrc@gmail.com>
2017-06-06 22:50:47 +0300

    student tagging feature on view-photo page

--- src/components/view-photo-component/view-photo-component.ts
+++ src/components/view-photo-component/view-photo-component.ts
@@ -8,36 +8,60 @@ import {InviteOthersPage} from "../invite-others/invite-others";
 import {Parents} from "../../providers/parents";
 import {ParentModel} from "../../models/parent-model";
 import {ImageModel} from "../../models/image-model";
+import {Gallery} from "../../providers/gallery";
 
 @Component({
-  selector: 'view-photo-component',
-  templateUrl: 'view-photo-component.html',
-    providers: [Translator]
+   selector: 'view-photo-component',
+   templateUrl: 'view-photo-component.html',
+   providers: [Translator]
 })
 
 
-export class ViewPhotoComponent implements OnInit{
-    @Input() photo: ImageModel;
-    @Input() classId: string;
-    private translate: TranslateService;
+export class ViewPhotoComponent implements OnInit {
+   @Input() photo: ImageModel;
+   @Input() classId: string;
+   private translate: TranslateService;
    private students: Promise<ParentModel[]>;
+   private isStudentTaggedDictionary: {};
 
 
-    constructor(public navCtrl: NavController,
-                public translator: Translator,
-                private parentProvider: Parents,
-    ) {
+   constructor(public navCtrl: NavController,
+               public translator: Translator,
+               private parentProvider: Parents,
+               private galleryProvider: Gallery,) {
 
-    }
+   }
 
-    ngOnInit(): void {
-       console.log('ViewPhotoComponent class id is:')
-       console.log(this.classId)
-        this.translate = this.translator.translatePipe;
-        this.students = this.parentProvider.getParentsOfClass(this.classId)
-       console.log('ViewPhotoComponent students:')
-       console.log(this.students)
-    }
+   ngOnInit(): void {
+      console.log('ViewPhotoComponent class id is:')
+      console.log(this.classId)
+      this.translate = this.translator.translatePipe;
+      this.students = this.parentProvider.getParentsOfClass(this.classId)
+      this.isStudentTaggedDictionary = {}
+      this.loadStudentsTaggedInThisImage()
+   }
 
+   private loadStudentsTaggedInThisImage() {
+      this.students.then(students => {
+         students.forEach(student => {
+            this.isStudentTagged(student.id).then(isTagged => {
+               this.isStudentTaggedDictionary[student.id] = isTagged
+            })
+         })
+      })
+   }
 
+   isStudentTagged(studentId) {
+      return this.galleryProvider.isStudentTaggedInImage(this.classId, this.photo.id, studentId)
+   }
+
+   tagStudent(studentId) {
+      this.galleryProvider.tagStudentInImage(this.classId, this.photo.id, studentId)
+      this.loadStudentsTaggedInThisImage()
+   }
+
+   untagStudent(studentId) {
+      this.galleryProvider.untagStudentInImage(this.classId, this.photo.id, studentId)
+      this.loadStudentsTaggedInThisImage()
+   }
 }
