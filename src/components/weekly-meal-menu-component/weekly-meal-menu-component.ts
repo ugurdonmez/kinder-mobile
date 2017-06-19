1cc17a0968b24786439aff8ede2f6e9586ac33bc
borakrc <borakrc@gmail.com>
2017-06-19 04:01:00 +0300

    users can now see all weekly menus in one page as a grid

--- src/components/weekly-meal-menu-component/weekly-meal-menu-component.ts
+++ src/components/weekly-meal-menu-component/weekly-meal-menu-component.ts
@@ -6,6 +6,7 @@ import {Translator} from "../../app/translator";
 import {TranslateService} from "ng2-translate";
 import {WeeklyMealMenu} from "../../providers/weeklymealmenu";
 import {Camera} from "ionic-native";
+import {WeeklyMealMenuModel} from "../../models/weekly-meal-menu-model";
 
 @Component({
    selector: 'weekly-meal-menu-component',
@@ -17,6 +18,7 @@ export class WeeklyMealMenuComponent implements OnInit {
    private next15Weeks: any[] = [];
    private translate: TranslateService;
    @Input() classId: string;
+   private weeklyMeals: any;
    
    constructor(private alertCtrl: AlertController,
                public translator: Translator,
@@ -29,6 +31,7 @@ export class WeeklyMealMenuComponent implements OnInit {
    ngOnInit(): void {
       this.translate = this.translator.translatePipe;
       this.getNext15Weeks()
+      this.loadWeeklyMeals()
    }
 
    addWeeklyMenuPhoto(){
@@ -55,15 +58,50 @@ export class WeeklyMealMenuComponent implements OnInit {
       alert.present();
    }
 
+   private getMealName(mealDate){
+      console.log('getMealName(mealDate: ')
+      console.log(mealDate)
+      mealDate = parseInt(mealDate)
+      console.log('getMealName week start for this date: ')
+      let weekStart = moment(mealDate).local().day(1).startOf('day').format('L')
+      console.log(weekStart)
+      let weekEnd = moment(mealDate).local().day(5).startOf('day').format('L')
+
+      return weekStart + ' - ' + weekEnd
+   }
+
    private getNext15Weeks():void {
       let counter = 0
       this.next15Weeks = []
       while (counter < 15){
          this.next15Weeks.push([
-            moment().day(1 + (counter*7)).valueOf(),
-            moment().local().day(1 + (counter*7)).calendar() + ' - ' + moment().local().day(5 + (counter*7)).calendar()
+            moment().day(1 + (counter*7)).startOf('day').valueOf(),
+            moment().local().day(1 + (counter*7)).startOf('day').format('L') +
+            ' - ' +
+            moment().local().day(5 + (counter*7)).startOf('day').format('L')
          ])
          counter += 1;
       }
+   }
+
+   private loadWeeklyMeals() {
+      let counter = 0
+      this.weeklyMeals = []
+      while (counter < 15){
+         this.weeklyMeals.push(
+            this.weeklyMealMenuProvider.getMenuImage(
+               this.classId,
+               moment().day(1 + (counter*7)).startOf('day').valueOf().toString()
+            )
+         )
+         counter += 1;
+      }
+   }
+
+   private openWeeklyMealPage(week: Promise<WeeklyMealMenuModel>){
+      week.then(subs => {
+         console.log('openWeeklyMealPage load:')
+         console.log(subs)
+      })
    }
 }
